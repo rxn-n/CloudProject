886cbb1f8573397d0c402ef201f54930b4f56cc1
rxn-n <48950662+rxn-n@users.noreply.github.com>
2025-02-18 18:43:18 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,12 +39,6 @@ export default function App() {
               path="/"
               element={
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                  <div className="lg:col-span-2">
-                    <TicketList />
-                  </div>
-                  <div>
-                    <NotificationLog />
-                  </div>
                 </div>
               }
             />
